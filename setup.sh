e1523f546fb2eabd7cf9b538bdb9a1d71e40e0f0
ajitrajasekharan <ajitrajasekharan@icloud.com>
2021-12-05 13:42:36 +0000

    Combined both service setups into one script

--- setup.sh
+++ setup.sh
@@ -4,3 +4,9 @@ pip install gdown
 echo "Fetching model"
 fetch_model.sh
 echo "Run run_server.sh to start service"
+
+echo "Settting up aux service for NER"
+pip install unidecode
+(cd POS_wrapper; make)
+echo "Change directory to POS_wrappaer to run_server.sh to also  start service required for NER"
+
