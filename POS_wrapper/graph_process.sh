d3b8abcc60a60861c31d0a314fd268505d57abb6
ajitrajasekharan <ajitrajasekharan@icloud.com>
2022-01-04 02:10:09 +0000

    Disabled minimal clean of text. This is to enable ner tokenization to be in sync with input

--- POS_wrapper/graph_process.sh
+++ POS_wrapper/graph_process.sh
@@ -1,3 +1,4 @@
+set -x
 sentence=${1?"Enter sentence"}
 to_lower=${2-0}
 bigram_gen=${3-1}
@@ -9,7 +10,8 @@ python conv_unicode.py "$sentence" > $tmp_file
 #cat $tmp_file > arg
 SERVER_URL=`python get_config.py`
 #cat arg
-./minimal_clean -lowercase $to_lower  -input $tmp_file -output $tmp_file2 > /dev/null
+#./minimal_clean -lowercase $to_lower  -input $tmp_file -output $tmp_file2 > /dev/null
+cp $tmp_file $tmp_file2
 sentence=`cat $tmp_file2`
 cp $tmp_file2 out_minimal_pre
 wget $SERVER_URL"$sentence" -O $tmp_file 2>/dev/null
