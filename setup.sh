119426de06a329776e18a167150cf884b24f84d0
ajitrajasekharan <ajitrajasekharan@icloud.com>
2022-01-22 15:23:25 +0000

    bug fix

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ pip install cython numpy
 pip install dynet==2.0.3
 pip install gdown
 echo "Fetching model"
-fetch_model.sh
+./fetch_model.sh
 echo "Run run_server.sh to start service"
 
 echo "Settting up aux service for NER"
